b00f19487cd3e003155e92e6bd37404a7ff53177
zac <z.lewis@base2services.com>
2019-09-25 13:16:20 +1000

    Set associations as top-level params.

--- waf.cfhighlander.rb
+++ waf.cfhighlander.rb
@@ -6,6 +6,11 @@ CfhighlanderTemplate do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', isGlobal: true
     ComponentParam 'AssociatedResourceArn', ''
+
+    # Web ACL associations
+    associations.each do |res_name, res_arn|
+      ComponentParam res_arn
+    end if defined?(associations)
   end
 
   LambdaFunctions 'custom_resource_functions' if defined? custom_resource_functions
